67fdc233447099077ebaa7b6ec341a6047a770de
BILAL HASSAN <expertofvision@gmail.com>
2021-01-13 23:20:36 +0000

    Add files via upload

---
+++ apprearance/full_body_2018.m
@@ -0,0 +1,47 @@
+function fv = full_body_2018(n)
+disp('now.................')
+disp(n)
+sub_dir = n;
+
+mkdir full_body_2018_gfd
+
+images_directory = ['full_body_2018/',num2str(sub_dir)];
+
+mkdir(sprintf('./full_body_2018_centered/%i', sub_dir));
+
+out_directory = ['full_body_2018_centered/',num2str(sub_dir)];
+
+
+
+gfd_all = [];
+images = dir(fullfile(images_directory,'*.png'));
+for idx = 1 : length(images)
+ image = imread(fullfile(images_directory,images(idx).name));
+ gray_image = rgb2gray(image);
+ bin_image = imbinarize(gray_image);
+ cc = bwconncomp(bin_image,8);
+ if cc.NumObjects == 1
+     centered_image = centerobject(bin_image);
+     image_name = sprintf('%d.jpg',idx);
+     fullFileName = fullfile(out_directory, image_name);
+     imwrite(centered_image,fullFileName);
+     gfd_features = gfd(centered_image, 3, 12);
+     gfd_all = [gfd_all, gfd_features];
+     disp('Next..................................................................')
+
+ end
+end
+
+%size(gfd_all);
+gfd_transpose = transpose(gfd_all);
+[r , c] = size(gfd_transpose);
+for row = 1: r
+    gfd_transpose(row, 53) = sub_dir;
+end
+
+save(['full_body_2018_gfd/' num2str(sub_dir) '.mat'] ,'gfd_transpose');
+fv = gfd_transpose;
+
+%disp('Done............................................')
+%return gfd_transpose
+end
